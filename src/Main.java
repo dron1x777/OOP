5af905fbbfc8ad390100c1930c2c04d513caf459
dron1x <usaamidin@gmail.com>
2026-01-28 17:19:05 +0600

    class work.

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car();
-        car1.color = "red";
-        car1.model = "AUDI";
-        car1.year = 2016;
-        Car car2 = new Car();
-        car2.color = "black";
-        car2.model = "BMW";
-        car2.year = 1998;
-        Car car3 = new Car();
-        car3.color = "grey";
-        car3.model = "MERS";
-        car3.year = 2019;
-        System.out.println(car1.color + " " + car1.model + " " + car1.year);
-        System.out.println(car2.color + " " + car2.model + " " + car2.year);
-        System.out.println(car3.color + " " + car3.model + " " + car3.year);
-
-
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        Programmer programmer = new Programmer();
+        programmer.name = sc.nextLine();
+        programmer.surname = sc.nextLine();
+        programmer.age = sc.nextInt();
+        System.out.println(programmer.name + " " +  programmer.surname + " " + programmer.age);
+        System.out.println(programmer.getDateOfBirth());
     }
 }
---
+++ src/Programmer.java
@@ -0,0 +1,9 @@
+public class Programmer {
+    String name;
+    String surname;
+    int age;
+
+    public int getDateOfBirth() {
+        return 2026-this.age;
+    }
+}

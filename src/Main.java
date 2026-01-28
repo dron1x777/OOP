b6875dc7d015c4e4264b4f1b0d242dc8b0dfeb0a
dron1x <usaamidin@gmail.com>
2026-01-28 17:46:15 +0600

    home work.

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        Programmer programmer = new Programmer();
-        programmer.name = sc.nextLine();
-        programmer.surname = sc.nextLine();
-        programmer.age = sc.nextInt();
-        System.out.println(programmer.name + " " +  programmer.surname + " " + programmer.age);
-        System.out.println(programmer.getDateOfBirth());
+        Triangle triangle = new Triangle();
+        triangle.a =  sc.nextInt();
+        triangle.b = sc.nextInt();
+        triangle.c = sc.nextInt();
+        System.out.println(triangle.Triangle());
 
     }
 }
---
+++ src/Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle {
+    int a;
+    int b;
+    int c;
+    public double Triangle() {
+        double p = (a + b + c) / 2;
+        double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return S;
+    }
+
+}
